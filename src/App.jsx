c1314b036915f756cc5b192bf93a52e079aa9b8f
78wolf <angieliuyz@gmail.com>
2024-04-09 17:28:12 -0400

    Add Experience & Education components in APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import General from "./components/General";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div>
       <h1>My CV Application</h1>
       <General />
+      <Education />
+      <Experience />
     </div>
   );
 }
